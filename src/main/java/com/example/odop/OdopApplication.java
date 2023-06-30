35ad6634e3331a99a8bdf23b8b1e1f1c7c9fe0b2
yeofriendship <yeofriendship@naver.com>
2023-06-30 17:24:18 +0900

    기본 설정 setting

---
+++ src/main/java/com/example/odop/OdopApplication.java
@@ -0,0 +1,13 @@
+package com.example.odop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/odop/OdopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.odop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
