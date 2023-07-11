8fb6c610350c6a3d930ecf21c828c517d6bbb493
9aramkim <rlarkfka0113@naver.com>
2023-07-11 17:31:25 +0900

    s

--- src/main/java/com/example/odop/OdopApplication.java
+++ src/main/java/com/example/odop/OdopApplication.java
@@ -7,25 +7,25 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
 @SpringBootApplication
-@EntityScan(basePackages = {"com.example.entity"})
+@EntityScan(basePackages = { "com.example.entity" })
 @EnableJpaRepositories(basePackages = {
-	"com.example.repository.AR", "com.example.repository.GR", "com.example.repository.MH",
-	"com.example.repository.SG", "com.example.repository.WJ"
+		"com.example.repository.AR", "com.example.repository.GR", "com.example.repository.MH",
+		"com.example.repository.SG", "com.example.repository.WJ"
 })
 @MapperScan(basePackages = {
-	"com.example.mapper.AR", "com.example.mapper.GR", "com.example.mapper.MH",
-	"com.example.mapper.SG", "com.example.mapper.WJ"
+		"com.example.mapper.AR", "com.example.mapper.GR", "com.example.mapper.MH",
+		"com.example.mapper.SG", "com.example.mapper.WJ"
 })
 @ComponentScan(basePackages = {
-	"com.example.service.AR", "com.example.service.GR", "com.example.service.MH",
-	"com.example.service.SG", "com.example.service.WJ", "com.example.service",
-	"com.example.controller.AR", "com.example.controller.GR", "com.example.controller.MH",
-	"com.example.controller.SG", "com.example.controller.WJ",
-	"com.example.restcontroller.AR", "com.example.restcontroller.GR", "com.example.restcontroller.MH",
-	"com.example.restcontroller.SG", "com.example.restcontroller.WJ",
-	"com.example.config", "com.example.filter"
+		"com.example.service.AR", "com.example.service.GR", "com.example.service.MH",
+		"com.example.service.SG", "com.example.service.WJ", "com.example.service",
+		"com.example.controller",
+		"com.example.controller.AR", "com.example.controller.GR", "com.example.controller.MH",
+		"com.example.controller.SG", "com.example.controller.WJ",
+		"com.example.restcontroller.AR", "com.example.restcontroller.GR", "com.example.restcontroller.MH",
+		"com.example.restcontroller.SG", "com.example.restcontroller.WJ",
+		"com.example.config", "com.example.filter"
 })
 public class OdopApplication {
 
