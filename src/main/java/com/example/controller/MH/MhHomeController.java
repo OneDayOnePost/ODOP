cae8c4925dba0928257673c868975df9b5e0b415
shmjo0604 <shmjo0604@gmail.com>
2023-07-12 17:30:01 +0900

    1
    1

--- src/main/java/com/example/controller/MH/MhHomeController.java
+++ src/main/java/com/example/controller/MH/MhHomeController.java
@@ -1,11 +1,21 @@
 package com.example.controller.MH;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MhHomeController {
-    
+
+    @Value("${google.clientId}") String clientId;
+    @Value("${google.login.url}") String loginUrl;
+    @Value("${google.redirect.url}") String redirectUrl;
+
+
+
     @GetMapping(value = "/mhlogin.do")
     public String loginGET() {
 
@@ -29,5 +39,16 @@ public class MhHomeController {
     public String test1GET() {
         return "/MH/Mhtest";
     }
-    
+
+    // 구글 로그인창 호출
+    @GetMapping(value = "/getGoogleAuthUrl")
+    public @ResponseBody String getGoogleAuthUrl(HttpServletRequest request) throws Exception {
+
+        String reqUrl = clientId + "/o/oauth2/v2/auth?client_id=" + loginUrl + "&redirect_uri="
+                + redirectUrl
+                + "&response_type=code&scope=email%20profile%20openid&access_type=offline";
+
+        return reqUrl;
+    }
+
 }
