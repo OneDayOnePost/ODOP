e01739df3cd7b689bda13bb10cce21cfbad1e62f
0hzL <arin980921@naver.com>
2023-07-13 17:31:15 +0900

    회원가입 1

--- src/main/java/com/example/controller/AR/ArHomeController.java
+++ src/main/java/com/example/controller/AR/ArHomeController.java
@@ -1,9 +1,40 @@
 package com.example.controller.AR;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.entity.Member;
 
 @Controller
 public class ArHomeController {
     
-    
+
+    // 회원가입
+    @GetMapping(value = "/join.do")
+    public String joinGET(){
+        try {
+            return "/AR/join";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "redirect:/odop/home.do";
+        }
+    }
+
+    @PostMapping(value = "/join.do")
+    public String joinPOST(@ModelAttribute Member member){
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    @GetMapping(value = "/login.do")
+    public String loginGET() {
+
+        return "/AR/login"; 
+
+    }
 }
