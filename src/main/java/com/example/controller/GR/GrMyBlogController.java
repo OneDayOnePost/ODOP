d115ee4dcb890f7926b5b90419120333d0cd5c2c
9aramkim <rlarkfka0113@naver.com>
2023-07-24 09:32:07 +0900

    1

--- src/main/java/com/example/controller/GR/GrMyBlogController.java
+++ src/main/java/com/example/controller/GR/GrMyBlogController.java
@@ -25,7 +25,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
-@Slf4j
+@Slf4j 
 @RequestMapping
 @RequiredArgsConstructor
 public class GrMyBlogController {
