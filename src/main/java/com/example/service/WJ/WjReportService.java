246590be0d088e2ddc06c33b406389e87432bbf0
yeofriendship <123910842+yeofriendship@users.noreply.github.com>
2023-07-27 23:21:24 +0900

    2023년 7월 27일

--- src/main/java/com/example/service/WJ/WjReportService.java
+++ src/main/java/com/example/service/WJ/WjReportService.java
@@ -1,10 +1,15 @@
 package com.example.service.WJ;
 
+import java.math.BigInteger;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Service;
 
+import com.example.dto.PostDTO;
 import com.example.dto.ReportListDTO;
+import com.example.dto.ReportOneDTO;
 
 @Service
 public interface WjReportService {
@@ -16,4 +21,13 @@ public interface WjReportService {
     public List<ReportListDTO> selectPostListWait();
 
     // 3. 삭제 완료
+    public List<ReportListDTO> selectPostListDelete();
+
+    // ------------------------------------------------------------------------
+
+    // 게시글 1개 조회
+    public PostDTO selectPostOne(@Param("no") BigInteger no);
+
+    // 게시글 신고 1개 상세 조회
+    public List<ReportOneDTO> selectPostReportOne(@Param("postno") BigInteger postno);
 }
