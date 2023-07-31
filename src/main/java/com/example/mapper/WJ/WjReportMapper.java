7cb0af6b61ac04588a3f1dfc87e610cc42085ccb
yeofriendship <123910842+yeofriendship@users.noreply.github.com>
2023-07-31 19:14:35 +0900

    2023년 7월 31일

--- src/main/java/com/example/mapper/WJ/WjReportMapper.java
+++ src/main/java/com/example/mapper/WJ/WjReportMapper.java
@@ -11,6 +11,7 @@ import org.apache.ibatis.annotations.Update;
 
 import com.example.dto.ReportOneDTO;
 import com.example.dto.PostDTO;
+import com.example.dto.ReplyDTO;
 import com.example.dto.ReportListDTO;
 
 @Mapper
@@ -50,7 +51,11 @@ public interface WjReportMapper {
 
     // 게시글 신고 삭제 거절
     @Delete({" DELETE FROM post_report WHERE postno = #{postno} "})
-    public int reportDelete(@Param("postno") BigInteger postno);
+    public int postReportDelete(@Param("postno") BigInteger postno);
+
+    // 게시글 삭제 취소
+    @Update({" UPDATE post SET state = 0 WHERE no = #{postno} "})
+    public int postDeleteCancel(@Param("postno") BigInteger postno);
 
     // --------------------------------------------------------------------
 
@@ -67,4 +72,31 @@ public interface WjReportMapper {
     // 3. 삭제 완료
     @Select({" SELECT rr.REPLYNO AS no, r.writer AS email, COUNT(*) AS reportcount FROM REPLY_REPORT rr LEFT JOIN REPLY r ON rr.REPLYNO = r.NO WHERE r.state = 1 GROUP BY rr.REPLYNO ORDER BY no DESC "})
     public List<ReportListDTO> selectReplyListDelete();
+
+    // --------------------------------------------------------------------
+
+    // 댓글 - 상세 조회
+    // 댓글 1개 조회
+    @Select({" SELECT no, writer, content, regdate, state FROM reply WHERE no=#{no} "})
+    public ReplyDTO selectReplyOne(@Param("no") BigInteger no);
+
+    // 댓글 신고 1개 상세 조회
+    @Select({" SELECT reason, COUNT(replyno) AS reportcount FROM " + 
+             " (SELECT rr2.NO, rr2.reason, rrr.replyno, rrr.reasonno FROM REPLY_REASON rr2 " + 
+             " LEFT JOIN (SELECT rr.replyno, rr.reasonno FROM REPLY_REPORT rr, REPLY r WHERE rr.replyno = r.NO) rrr " + 
+             " ON rr2.NO = rrr.reasonno AND rrr.replyno = #{replyno}) " + 
+             " GROUP BY reason ORDER BY NO ASC "})
+    public List<ReportOneDTO> selectReplyReportOne(@Param("replyno") BigInteger replyno);
+
+    // 댓글 신고 삭제 승인
+    @Update({" UPDATE reply SET state = 1 WHERE no = #{replyno} "})
+    public int replyDelete(@Param("replyno") BigInteger replyno);
+
+    // 댓글 신고 삭제 거절
+    @Delete({" DELETE FROM reply_report WHERE replyno = #{replyno} "})
+    public int replyReportDelete(@Param("replyno") BigInteger replyno);
+
+    // 댓글 삭제 취소
+    @Update({" UPDATE reply SET state = 0 WHERE no = #{replyno} "})
+    public int replyDeleteCancel(@Param("replyno") BigInteger replyno);
 }
