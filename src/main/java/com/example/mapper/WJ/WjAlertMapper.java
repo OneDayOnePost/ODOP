cd7eaa3c00b03986206d5d9ac5455137fcb759be
서명호(Myungho Seo) <shmjo0604@gmail.com>
2023-08-29 23:08:45 +0900

    23.08.29
    댓글 알림 성공...!

--- src/main/java/com/example/mapper/WJ/WjAlertMapper.java
+++ src/main/java/com/example/mapper/WJ/WjAlertMapper.java
@@ -10,6 +10,7 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
+import com.example.dto.AlertDTO;
 import com.example.entity.Alert;
 
 @Mapper
@@ -17,6 +18,13 @@ public interface WjAlertMapper {
     // 알림 추가
     @Insert({" INSERT INTO ALERT (email, content, type, url, regdate) VALUES (#{email}, #{content}, #{type}, #{url}, #{regdate}) "})
     public int alertInsert(@Param("email") String email, @Param("content") String content, @Param("type") String type, @Param("url") String url, @Param("regdate") Date regdate);
+
+    // 알림 추가 ( DTO 타입으로 파라미터 전달 )
+    @Insert({
+        " INSERT INTO ALERT (email, content, type, url) ",
+        " VALUES (#{alert.email}, #{alert.content}, #{alert.type}, #{alert.url})"
+    })
+    public int alertInsert2(@Param("alert") AlertDTO alert);
 
     // 알림 조회 (한 달 이내)
     @Select({" SELECT * FROM ALERT WHERE email = #{email} AND regdate >= DATEADD('MONTH', -1, CURRENT_TIMESTAMP) ORDER BY no DESC "})
@@ -33,4 +41,16 @@ public interface WjAlertMapper {
     // 팔로워 목록
     @Select({" SELECT myid FROM follow WHERE yourid = #{email} "})
     public List<String> selectFollowerList(@Param("email") String email);
+
+    // 댓글 알림 존재 확인 ( 있으면 update 실행, 없으면 insert 실행 )
+    @Select({
+        " SELECT COUNT(*) FROM alert WHERE type = #{alert.type} AND url = #{alert.url} "
+    })
+    public int selectReplyAlertCount(@Param("alert") AlertDTO alert);
+
+    // 댓글 알림 최신화
+    @Update({
+        " UPDATE alert SET content = #{alert.content}, chk = 0 WHERE type = #{alert.type} AND url = #{alert.url} "
+    })
+    public int updateReplyAlert(@Param("alert") AlertDTO alert);
 }
